06a1e9b7714c0338129cf565525d2e7f76af5826
serrodale <serrodale@gmail.com>
2020-10-04 20:27:01 +0200

    docs: adds example as documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+## Example
+
+```sh
+$ git clone git@github.com:serrodale/tstream.git
+$ npm i
+$ npm example
+```
---
+++ example.ts
@@ -0,0 +1,30 @@
+import { Stream } from '.';
+
+const getValuesFromDB = (): Promise<number[]> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([1, 2, 2, 4]);
+    }, 1000);
+  });
+};
+
+const filterEvenNumbers = (x: number): Promise<boolean> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x % 2 === 0);
+    }, 1000);
+  });
+};
+
+const main = async () => {
+  const output = await Stream
+    .of(getValuesFromDB())
+    .map(x => x * 2)
+    .filter(filterEvenNumbers)
+    .unique()
+    .sum();
+
+  console.log(output);
+};
+
+main();
